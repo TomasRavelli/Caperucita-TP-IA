e744929a16573e5f5eaafaed982fa1422fbca114
Gaston <gaston.diaz.contractor@macmillan.com>
2021-04-11 11:29:28 -0300

    Acción IrAbajo

--- src/tp/caperucita/search/caperucita/acciones/IrAbajo.java
+++ src/tp/caperucita/search/caperucita/acciones/IrAbajo.java
@@ -4,12 +4,39 @@ import frsf.cidisi.faia.agent.search.SearchAction;
 import frsf.cidisi.faia.agent.search.SearchBasedAgentState;
 import frsf.cidisi.faia.state.AgentState;
 import frsf.cidisi.faia.state.EnvironmentState;
+import tp.caperucita.search.ambiente.AmbienteEstado;
+import tp.caperucita.search.auxiliar.PosicionCelda;
+import tp.caperucita.search.caperucita.CaperucitaEstado;
 
 public class IrAbajo extends SearchAction{
 
 	@Override
 	public SearchBasedAgentState execute(SearchBasedAgentState s) {
-		// TODO Auto-generated method stub
+
+		CaperucitaEstado estadoCaperucita = (CaperucitaEstado) s;
+		PosicionCelda nuevaPosicion = new PosicionCelda(), posicionActual = estadoCaperucita.getPosicionActual();
+		int cantidadCeldasAbajo = estadoCaperucita.getCantidadCeldasAbajo();
+		int cantidadDulces = estadoCaperucita.getCantidadDulcesAbajo();
+		boolean hayLobo = estadoCaperucita.getHayLoboAbajo();;
+
+		if(cantidadCeldasAbajo > 0 && cantidadDulces == 0){
+			//Se cumplen las precondiciones
+			if(hayLobo){
+				//Está el lobo, entonces pierde una vida y vuelve al principio.
+				nuevaPosicion.setPosicionFila(0);
+				nuevaPosicion.setPosicionColumna(0);
+				estadoCaperucita.setPosicionActual(nuevaPosicion);
+				estadoCaperucita.setCantidadVidas(estadoCaperucita.getCantidadVidas() - 1);
+				estadoCaperucita.setCantidadDulces(0);
+				return estadoCaperucita;
+			}
+			//no hay lobo, entonces se mueve.
+			nuevaPosicion.setPosicionColumna(posicionActual.getPosicionColumna()); //La columna no cambia
+			nuevaPosicion.setPosicionFila(posicionActual.getPosicionFila() + cantidadCeldasAbajo); //Se mueve cantidadCeldasAbajo celdas haica abajo (sumando)
+			estadoCaperucita.setPosicionActual(nuevaPosicion);
+			return estadoCaperucita;
+		}
+		//No se cumplen las precondiciones, esta acción no es válida
 		return null;
 	}
 
@@ -21,14 +48,44 @@ public class IrAbajo extends SearchAction{
 
 	@Override
 	public EnvironmentState execute(AgentState ast, EnvironmentState est) {
-		// TODO Auto-generated method stub
+
+		CaperucitaEstado estadoCaperucita = (CaperucitaEstado) ast;
+		AmbienteEstado estadoAmbiente = (AmbienteEstado) est;
+		PosicionCelda nuevaPosicion = new PosicionCelda(), posicionActual = estadoCaperucita.getPosicionActual();
+		int cantidadCeldasAbajo = estadoCaperucita.getCantidadCeldasAbajo();
+		int cantidadDulces = estadoCaperucita.getCantidadDulcesAbajo();
+		boolean hayLobo = estadoCaperucita.getHayLoboAbajo();;
+
+		if(cantidadCeldasAbajo > 0 && cantidadDulces == 0){
+			//Se cumplen las precondiciones
+			if(hayLobo){
+				//Está el lobo, entonces pierde una vida y vuelve al principio.
+				nuevaPosicion.setPosicionFila(0);
+				nuevaPosicion.setPosicionColumna(0);
+				estadoCaperucita.setPosicionActual(nuevaPosicion);
+				estadoCaperucita.setCantidadVidas(estadoCaperucita.getCantidadVidas() - 1);
+				estadoCaperucita.setCantidadDulces(0);
+				//Actualizo el ambiente
+				estadoAmbiente.setPosicionCaperucita(nuevaPosicion);
+
+				return estadoAmbiente;
+			}
+			//no hay lobo, entonces se mueve.
+			nuevaPosicion.setPosicionColumna(posicionActual.getPosicionColumna()); //La columna no cambia
+			nuevaPosicion.setPosicionFila(posicionActual.getPosicionFila() + cantidadCeldasAbajo); //Se mueve cantidadCeldasAbajo celdas haica abajo (sumando)
+			estadoCaperucita.setPosicionActual(nuevaPosicion);
+			//Actualizo el ambiente
+			estadoAmbiente.setPosicionCaperucita(nuevaPosicion);
+
+			return estadoAmbiente;
+		}
+		//No se cumplen las precondiciones, esta acción no es válida
 		return null;
 	}
 
 	@Override
 	public String toString() {
-		// TODO Auto-generated method stub
-		return null;
+		return "Ir abajo";
 	}
 
 }
