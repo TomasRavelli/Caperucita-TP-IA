4966989a9aecbaca64013b62801275915282358f
MalenaMoix <moixmalena@gmail.com>
2021-04-11 18:26:03 -0300

    Implementacion de las acciones IrDerecha e IrDerechaYJuntarDulce

--- src/tp/caperucita/search/caperucita/acciones/IrDerecha.java
+++ src/tp/caperucita/search/caperucita/acciones/IrDerecha.java
@@ -4,12 +4,68 @@ import frsf.cidisi.faia.agent.search.SearchAction;
 import frsf.cidisi.faia.agent.search.SearchBasedAgentState;
 import frsf.cidisi.faia.state.AgentState;
 import frsf.cidisi.faia.state.EnvironmentState;
+import tp.caperucita.search.ambiente.AmbienteEstado;
+import tp.caperucita.search.auxiliar.PosicionCelda;
+import tp.caperucita.search.caperucita.CaperucitaEstado;
 
 public class IrDerecha extends SearchAction {
 
 	@Override
 	public SearchBasedAgentState execute(SearchBasedAgentState s) {
-		// TODO Auto-generated method stub
+		CaperucitaEstado estadoCaperucita = (CaperucitaEstado) s;
+		int cantidadCeldasLibreDerecha = estadoCaperucita.getCantidadCeldasDerecha();
+		PosicionCelda posicionActual = estadoCaperucita.getPosicionActual();
+		PosicionCelda nuevaPosicion = new PosicionCelda();
+		int cantidadDulcesDerecha = estadoCaperucita.getCantidadDulcesDerecha();
+		boolean hayLoboDerecha = estadoCaperucita.getHayLoboDerecha();
+
+		if (cantidadCeldasLibreDerecha > 0 && cantidadDulcesDerecha == 0) {
+			if (!hayLoboDerecha) {
+				nuevaPosicion.setPosicionColumna(posicionActual.getPosicionColumna() + cantidadCeldasLibreDerecha);
+				nuevaPosicion.setPosicionFila(posicionActual.getPosicionFila());
+				estadoCaperucita.setPosicionActual(nuevaPosicion);
+				return estadoCaperucita;
+			}
+			//Esta el lobo, caperucita pierde una vida y todos los dulces.
+			nuevaPosicion.setPosicionColumna(0);
+			nuevaPosicion.setPosicionFila(0);
+			estadoCaperucita.setCantidadDulces(0);
+			estadoCaperucita.setPosicionActual(nuevaPosicion);
+			estadoCaperucita.setCantidadVidas(estadoCaperucita.getCantidadVidas()-1);
+			return estadoCaperucita;
+		}
+
+		return null;
+	}
+
+	@Override
+	public EnvironmentState execute(AgentState ast, EnvironmentState est) {
+		CaperucitaEstado estadoCaperucita = (CaperucitaEstado) ast;
+		PosicionCelda nuevaPosicion = new PosicionCelda();
+		PosicionCelda posicionActual = estadoCaperucita.getPosicionActual();
+		int cantidadDulcesDerecha = estadoCaperucita.getCantidadDulcesDerecha();
+		int cantidadCeldasLibreDerecha = estadoCaperucita.getCantidadCeldasDerecha();
+		boolean hayLoboDerecha = estadoCaperucita.getHayLoboDerecha();
+		AmbienteEstado estadoAmbiente = (AmbienteEstado) est;
+
+		if (cantidadCeldasLibreDerecha > 0 && cantidadDulcesDerecha == 0) {
+			if (!hayLoboDerecha) {
+				nuevaPosicion.setPosicionColumna(posicionActual.getPosicionColumna() + cantidadCeldasLibreDerecha);
+				nuevaPosicion.setPosicionFila(posicionActual.getPosicionFila());
+				estadoCaperucita.setPosicionActual(nuevaPosicion);
+				estadoAmbiente.setPosicionCaperucita(estadoCaperucita.getPosicionActual());
+				return estadoAmbiente;
+			}
+			//Esta el lobo, caperucita pierde una vida y todos los dulces
+			estadoCaperucita.setCantidadDulces(0);
+			posicionActual.setPosicionColumna(0);
+			posicionActual.setPosicionFila(0);
+			estadoCaperucita.setPosicionActual(posicionActual);
+			estadoCaperucita.setCantidadVidas(estadoCaperucita.getCantidadVidas() - 1);
+			estadoAmbiente.setPosicionCaperucita(estadoCaperucita.getPosicionActual());
+			return estadoAmbiente;
+		}
+
 		return null;
 	}
 
@@ -20,15 +76,9 @@ public class IrDerecha extends SearchAction {
 	}
 
 	@Override
-	public EnvironmentState execute(AgentState ast, EnvironmentState est) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
 	public String toString() {
 		// TODO Auto-generated method stub
-		return null;
+		return "Ir Derecha";
 	}
 
 }
--- src/tp/caperucita/search/caperucita/acciones/IrDerechaYJuntarDulce.java
+++ src/tp/caperucita/search/caperucita/acciones/IrDerechaYJuntarDulce.java
@@ -4,12 +4,70 @@ import frsf.cidisi.faia.agent.search.SearchAction;
 import frsf.cidisi.faia.agent.search.SearchBasedAgentState;
 import frsf.cidisi.faia.state.AgentState;
 import frsf.cidisi.faia.state.EnvironmentState;
+import tp.caperucita.search.ambiente.AmbienteEstado;
+import tp.caperucita.search.auxiliar.PosicionCelda;
+import tp.caperucita.search.caperucita.CaperucitaEstado;
 
 public class IrDerechaYJuntarDulce extends SearchAction{
 
 	@Override
 	public SearchBasedAgentState execute(SearchBasedAgentState s) {
-		// TODO Auto-generated method stub
+		CaperucitaEstado estadoCaperucita = (CaperucitaEstado) s;
+		PosicionCelda nuevaPosicion = new PosicionCelda();
+		PosicionCelda posicionActual = estadoCaperucita.getPosicionActual();
+		int cantCeldasDerecha = estadoCaperucita.getCantidadCeldasDerecha();
+		int cantDulcesDerecha = estadoCaperucita.getCantidadDulcesDerecha();
+		boolean hayLoboDerecha = estadoCaperucita.getHayLoboDerecha();
+
+		nuevaPosicion.setPosicionFila(posicionActual.getPosicionFila());
+		nuevaPosicion.setPosicionColumna(posicionActual.getPosicionColumna() + cantCeldasDerecha);
+
+		if (cantCeldasDerecha > 0 && cantDulcesDerecha > 0) {
+			if (hayLoboDerecha) {
+				estadoCaperucita.setCantidadDulces(0);
+				estadoCaperucita.setPosicionActual(new PosicionCelda(0, 0));
+				estadoCaperucita.setCantidadVidas(estadoCaperucita.getCantidadVidas() - 1);
+				return estadoCaperucita;
+			}
+
+			estadoCaperucita.setCantidadDulces(estadoCaperucita.getCantidadDulces() + cantDulcesDerecha);
+			estadoCaperucita.setPosicionActual(nuevaPosicion);
+			return estadoCaperucita;
+		}
+
+		return null;
+	}
+
+	@Override
+	public EnvironmentState execute(AgentState ast, EnvironmentState est) {
+		CaperucitaEstado estadoCaperucita = (CaperucitaEstado) ast;
+		AmbienteEstado estadoAmbiente = (AmbienteEstado) est;
+		PosicionCelda nuevaPosicion = new PosicionCelda();
+		PosicionCelda posicionActual = estadoCaperucita.getPosicionActual();
+		int cantCeldasDerecha = estadoCaperucita.getCantidadCeldasDerecha();
+		int cantDulcesDerecha = estadoCaperucita.getCantidadDulcesDerecha();
+		boolean hayLoboDerecha = estadoCaperucita.getHayLoboDerecha();
+
+		nuevaPosicion.setPosicionFila(posicionActual.getPosicionFila());
+		nuevaPosicion.setPosicionColumna(posicionActual.getPosicionColumna() + cantCeldasDerecha);
+
+		if (cantCeldasDerecha > 0 && cantDulcesDerecha > 0) {
+			if (hayLoboDerecha) {
+				estadoCaperucita.setCantidadDulces(0);
+				estadoCaperucita.setPosicionActual(new PosicionCelda(0, 0));
+				estadoCaperucita.setCantidadVidas(estadoCaperucita.getCantidadVidas() - 1);
+				estadoAmbiente.setPosicionCaperucita(estadoCaperucita.getPosicionActual());
+				return estadoAmbiente;
+			}
+
+			estadoCaperucita.setCantidadDulces(estadoCaperucita.getCantidadDulces() + cantDulcesDerecha);
+			estadoCaperucita.setPosicionActual(nuevaPosicion);
+
+			estadoAmbiente.eliminarDulcesEnCamino(posicionActual, nuevaPosicion);
+			estadoAmbiente.setPosicionCaperucita(estadoCaperucita.getPosicionActual());
+			return estadoAmbiente;
+		}
+
 		return null;
 	}
 
@@ -20,15 +78,9 @@ public class IrDerechaYJuntarDulce extends SearchAction{
 	}
 
 	@Override
-	public EnvironmentState execute(AgentState ast, EnvironmentState est) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
 	public String toString() {
 		// TODO Auto-generated method stub
-		return null;
+		return "Ir derecha y juntar dulces";
 	}
 
 }
